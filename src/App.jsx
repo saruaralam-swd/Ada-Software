d16be93574396da984fd8647a4b1002eb90fa5f3
saruaralam-swd <saruaralam2001@gmail.com>
2023-09-08 12:19:15 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-semibold text-center text-red-400">
+        Hello Tailwind css
+      </h1>
+    </>
+  );
+}
+
+export default App;
